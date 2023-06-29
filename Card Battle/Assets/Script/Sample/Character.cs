1aef6ab942a9e2af0151b2831d8696031f24c05e
ProPim <qusdnals3@naver.com>
2023-06-29 15:12:27 +0900

    [feat]캐릭터 클래스 작성 #24

--- Card Battle/Assets/Script/Sample/Character.cs
+++ Card Battle/Assets/Script/Sample/Character.cs
@@ -6,20 +6,30 @@ public class Character : MonoBehaviour
 {
     public DefaultCharacter character;
 
+    //마왕은 여기다 포함을 할건지 아니면 다른 클래스를 만들건지 확인
+
     public string name;
+    public int id; // 캐릭터인지 마왕인지
     public int level;
     public int defense;
     public int hp;
     public int attackDmg;
 
     public WeaponType weapon;
-    public Sprite img;
+    public Sprite img; // 캐릭터 이미지
 
-    int id = 0;
+    
 
-    List<Card> cards;
+    List<Card> cards; //플레이어 덱 확인
 
+    public void Start()
+    {
+        Init();
+        Test();//작동되는 확인
+    }
     //Init은 나중에 Loader에서 한번에
+
+    
     public void Init()
     {
         cards = new List<Card>();
