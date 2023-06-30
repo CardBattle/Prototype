bcbff40a1569361fa069da75dad83703b7518477
RudinP <song_e@sookmyung.ac.kr>
2023-06-30 16:02:10 +0900

    [feat] 실제 CardUse 구현(#38)
    - CardUse의 가상함수 Use에 상대방 hp를 감소시키는 함수 작성
    - CardUse.cs에 CalculateDmg.cs 추가
    
    Close #38

--- Card Battle/Assets/Script/Delegate/Card/CardUse.cs
+++ Card Battle/Assets/Script/Delegate/Card/CardUse.cs
@@ -11,6 +11,7 @@ public class CardUse : MonoBehaviour
 
     public virtual void Use(Character sender, Character receiver)
     {
+        //버프 - 프로토타입 구현 X 연동만 해둠.
         if(card.card.buffs.Count > 0 )
         {
             foreach(var buff in card.card.buffs)
@@ -18,6 +19,13 @@ public class CardUse : MonoBehaviour
                 buff.buff.use(sender, receiver);
             }
         }
+
+        receiver.character.Hp -= CaculateDmg(sender.character.AttackDmg, card.card.Dice, 1);//후에 1=>effectiveness 계산 함수로 변경
+    }
+
+    int CaculateDmg(int attackDmg, int dice, float effectiveness)
+    {
+        return (int)Mathf.Round((attackDmg + dice) * effectiveness);
     }
 
 }
