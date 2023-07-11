1889ca17aa1b3c4cc638379a290f980b22282f0b
RudinP <song_e@sookmyung.ac.kr>
2023-07-11 10:22:32 +0900

    DefaultCharacter 및 Character에 List<Buff> 추가(#71)
    ;

--- Card Battle/Assets/Script/Class/Character.cs
+++ Card Battle/Assets/Script/Class/Character.cs
@@ -22,19 +22,15 @@ public class Character : MonoBehaviour
     private WeaponType weapon;
     [SerializeField]
     private Sprite img;
+    
+    public List<Buff> buffs;
 
     public List<GameObject> cards; //카드 매니저에서 캐릭터가 소유한 카드 프리팹을 접근해야하기 때문에 public
     
     public void Init()
     {
-        info = new(id, _name, level, hp, attackDmg, defense, cards, weapon, img);
+        buffs = new List<Buff>();
+        info = new(id, _name, level, hp, attackDmg, defense, cards, buffs, weapon, img);
     }
 
-    public void Test()
-    {
-        Debug.Log($"id: {info.Id}\nname: {info.Name}\n" +
-            $"level: {info.Level}\nhp: {info.Hp}\n" +
-            $"attackDmg: {info.AttackDmg}\ndefense: {info.Defense}\n"+
-            $"weapon: {info.Weapon}\nimg: {info.Img}");
-    }
 }
